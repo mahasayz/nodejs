de430d07de639d83cced279a41c10b2f9f140fff
Mahbub Alam <mahasayz@gmail.com>
2015-06-09 03:38:13 -0700

    User mouse broadcasting

---
+++ 10.server.js
@@ -0,0 +1,66 @@
+function handleHTTP(req, res) {
+
+	if (req.method === "GET") {
+		if (/^\/\d+(?=$|[\/?#])/.test(req.url)) {
+			req.addListener("end", function() {
+				req.url = req.url.replace(/^\/(\d+).*$/,"/$1.html");
+				static_files.serve(req,res);
+			});
+			req.resume();
+		} else if(req.url === "/jquery.js") {
+			static_files.serve(req,res);
+		} else {
+			res.writeHead(403);
+			res.end("Get outta Here");
+		}
+	} else {
+		res.writeHead(403);
+		res.end("Get outta Here");
+	}
+
+}
+
+function handleIO(socket) {
+	function disconnect() {
+		// clearInterval(intv);
+		console.log("Client Disconnected");
+	}
+
+	console.log("Client COnnected");
+
+	socket.on("disconnect", disconnect);
+
+	socket.on("spy", function(x, y) {
+		socket.broadcast.emit("spy", {
+			"x": x,
+			"y": y
+		})
+	})
+}
+
+var host = "localhost";
+var port = "8006";
+
+var http = require("http");
+var http_serv = http.createServer(handleHTTP).listen(port,host);
+
+var ASQ = require("asynquence");
+require("asynquence-contrib");
+
+var node_static = require("node-static");
+var static_files = new node_static.Server(__dirname);
+
+var io = require("socket.io").listen(http_serv);
+
+io.on("connection", handleIO);
+
+io.configure(function() {
+	io.enable("browser client minification");
+	io.enable("browser client etag");	// apply etag caching
+	io.set("log level", 1);		// reduce logging
+	io.set("transports", [
+		"websocket",
+		"xhr-polling",
+		"jsonp-polling"
+		]);
+});
